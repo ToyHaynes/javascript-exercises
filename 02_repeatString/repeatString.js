7903733dd10ad2679023680b53d04b4f55726c62
ToyHaynes <137526514+ToyHaynes@users.noreply.github.com>
2023-07-02 16:44:32 +0300

    done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let i = 0;
+    let result = "";
 
-};
+    if (num < 0) return "ERROR";
+
+    else {
+        while(i<num) {
+            result += string;
+            i++
+        }
+        return result
+    }
+}
 
 // Do not edit below this line
 module.exports = repeatString;
