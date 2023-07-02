ec62d26c50a7fcfd9b7dccbbfea28608162c9bce
ToyHaynes <137526514+ToyHaynes@users.noreply.github.com>
2023-07-02 18:55:56 +0300

    done

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let result = (degree - 32)*5/9;
+  result = +result.toFixed(1);
+  return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let result = (degree * 9/5) + 32;
+  result = +result.toFixed(1);
+  return result;
 };
 
 // Do not edit below this line
