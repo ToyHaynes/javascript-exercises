82f5023938e736f91efce188122fff6232cb7c46
ToyHaynes <137526514+ToyHaynes@users.noreply.github.com>
2023-07-02 18:22:04 +0300

    done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    let result = 0;
 
+    if ( !Number.isInteger(first) || !Number.isInteger(last) || first < 0 || last < 0) {
+        return "ERROR";
+
+    } else if (first > last) {
+            [first, last] = [last, first]
+    }
+
+    for (let i = first; i <= last; i++) {
+            result += i;
+        }
+    
+        return result
 };
 
 // Do not edit below this line
