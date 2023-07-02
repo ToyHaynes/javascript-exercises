81db7228b22ed6707533d56385d802a8fbf5244d
ToyHaynes <137526514+ToyHaynes@users.noreply.github.com>
2023-07-02 17:24:17 +0300

    done

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...theArgs) {
+    var i = 0;
 
-};
+    while (i < array.length) {
+       if (theArgs.includes(array[i])) {
+        array.splice(i, 1);
+       } else {
+        ++i;
+       }
+    }
+    return array;
+  }
 
 // Do not edit below this line
 module.exports = removeFromArray;
