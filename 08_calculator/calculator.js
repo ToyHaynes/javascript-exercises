6c4d4d6b5bb5873852c8b614cc85be53736b4235
ToyHaynes <137526514+ToyHaynes@users.noreply.github.com>
2023-07-05 17:59:46 +0300

    done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b
+}
+
+const sum = function(array) {
+  return array.reduce((total, currentNumber) => {return total + currentNumber;}, 0);
 };
 
-const sum = function() {
-	
+const multiply = function(...a) {
+  return a.reduce((total, currentNumber) => {
+    return total * currentNumber;
+  }, a);
 };
 
-const multiply = function() {
-
+const power = function(a,b) {
+  let power = a
+	for(let i =1; i<b; i++) {
+    power *= a
+  } 
+  return power
 };
 
-const power = function() {
-	
+const factorial = function(number) {
+  let total = 1;
+  for(let i=1; i<=number; i++){
+    total *= i
+  } return total
 };
 
-const factorial = function() {
-	
-};
-
+const calculator = { add, subtract, sum, multiply, power, factorial }
+ 
 // Do not edit below this line
 module.exports = {
   add,
