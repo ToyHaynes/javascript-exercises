3cd57c507b928c4126efb2c6e8bbc3a8c16375e5
ToyHaynes <137526514+ToyHaynes@users.noreply.github.com>
2023-07-05 19:54:01 +0300

    done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let fib = [1,1];
+    number = Number(number);
 
-};
+    if (number < 0) {
+        return "OOPS";
+    } else if (number >= 1) {
+        for(i=2; i<number; i++) {
+            fib[i] = (fib[i-1] + fib[i-2]);   
+    }return fib[number-1]
+    };
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
