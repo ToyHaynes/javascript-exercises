6e9d5b2ce7bf5fbe3683268f9081b9d5b2ac4d79
ToyHaynes <137526514+ToyHaynes@users.noreply.github.com>
2023-07-05 19:15:33 +0300

    done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
-};
+const palindromes = function (word) {
+    // Remove non-alphanumeric characters and convert to lowercase
+    const alphanumericWord = word.toLowerCase().replace(/[^a-z0-9]/g, '');
+  
+    // Reverse the string
+    const reversedWord = alphanumericWord.split('').reverse().join('');
+  
+    // Compare the reversed string with the original string
+    return alphanumericWord === reversedWord;
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
